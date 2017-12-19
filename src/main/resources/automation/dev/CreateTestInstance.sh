2dcbc66f025d24e0a413233b05f26facf9ca787e
nus889 <Likhith.Matta@capitalone.com>
2017-12-19 15:53:28 -0600

    Removing the automation from Repo

--- src/main/resources/automation/dev/CreateTestInstance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-StackName=$1
-wget https://raw.githubusercontent.com/likhith-3399/IntuitCodingChallange/master/src/main/resources/automation/ec2_cloudformation.json
-
-aws cloudformation create-stack --stack-name $StackName --region us-east-1 --template-body file://./ec2_cloudformation.json
--- src/main/resources/automation/dev/deploymentWrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-stackName=$1
-echo "Dependencies will not be installed with this script. Run InstallDependencies.sh if host machine is missing programs."
-./CreateStack.sh $stackName
-echo "Invoking Cloud Formation Script......"
